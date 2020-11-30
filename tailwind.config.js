9dc719723ae60b93cfba4a66d62aa69276ea8b64
Diov5 <claudio.valentino46@gmail.com>
2020-11-30 18:52:54 +0700

    mebuat section what we offer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      // Build your palette here
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.lightBlue,
+      yellow: colors.amber,
+    }
   },
   variants: {
     extend: {},
