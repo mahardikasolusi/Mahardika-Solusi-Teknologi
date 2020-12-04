f6af5bf2f08c8851f61de324e5bc329af85bfe7c
irfanmmr <irfanmaulanamr@student.upi.edu>
2020-12-04 17:18:43 +0700

    responsive

--- public/js/controller.js
+++ public/js/controller.js
@@ -17,8 +17,8 @@ $('.myteam').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 2,
+                slidesToScroll: 2,
                 infinite: true,
                 dots: true
             }
