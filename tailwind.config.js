d56a334c8bf197337c16716c7fa21963782e8629
unknown <inprapoe@gmail.com>
2021-05-03 22:29:26 +0700

    slicing baru

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      // Build your palette here
-      gray: colors.trueGray,
-      red: colors.red,
-      blue: colors.lightBlue,
-      yellow: colors.amber,
-    }
+    extend: {
+      colors: {
+        mst: {
+          blue: {
+            lighter: '#00C4DB',
+            DEFAULT: '#00ADC2',
+            darker: '#0096A8'
+          },
+          orange: {
+            lighter: '#FF891F',
+            DEFAULT: '#FF7A03',
+            darker: '#EB6F00'
+          }
+        }
+      }
+    },
   },
   variants: {
     extend: {},
